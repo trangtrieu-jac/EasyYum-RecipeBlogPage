e737f802e6bf7ec0215825a88e39cee3473a29b9
trangtrieu-jac <6341464@johnabbottcollege.net>
2025-02-05 22:55:26 -0500

    add case 10

---
+++ script/case10-burger.js
@@ -0,0 +1,21 @@
+const ctx = document.getElementById('nutrientChart').getContext('2d');
+  const nutrientData = {
+    labels: ['Protein', 'Fat', 'Carbohydrates'],
+    datasets: [{
+      label: 'Burger Nutritional Facts (Per Serving)',
+      data: [30, 25, 40],  // Example nutritional data (Calories, Protein, Fat, Carbohydrates)
+      backgroundColor: [
+        'rgb(255, 99, 132)',  // Calories (Red)
+        'rgb(54, 162, 235)',  // Protein (Blue)
+        'rgb(255, 205, 86)',  // Fat (Yellow)
+      ],
+      hoverOffset: 4
+    }]
+  };
+
+  const config = {
+    type: 'doughnut',
+    data: nutrientData,
+  };
+
+  const nutrientChart = new Chart(ctx, config);
